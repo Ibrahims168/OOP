5ab1176713cbf7c72d65f34bf4491e61b809894b
ibrahims168 <ibrahh881@gmail.com>
2022-02-24 15:52:48 +0200

    constructor

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 class User {
+    constructor(firstName,lastName,age,userName,type,password){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.userName = userName
+        this.password = password
+        this.age = age
+        this.type = type
+        this.isLoggedIn = false 
+    }
 
-    
 }
