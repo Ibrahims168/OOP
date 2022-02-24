95453d452663fd22202f367b1050c7acfe290d58
ibrahims168 <ibrahh881@gmail.com>
2022-02-24 15:59:39 +0200

    data infupt

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ class User {
     constructor(firstName,lastName,age,userName,type,password){
         this.firstName = firstName
         this.lastName = lastName
-        this.userName = userName
-        this.password = password
         this.age = age
+        this.userName = userName
         this.type = type
+        this.password = password
         this.isLoggedIn = false 
+        this.isActive = true
     }
 
 }
+
+const user = new User ('Ibrahim','Sharif','22','Isafadi','student','password123')
+
+console.log(user);
