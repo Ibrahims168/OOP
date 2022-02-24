6547f36369ce0fa8aa490f32c88330f0b7215ebe
ibrahims168 <ibrahh881@gmail.com>
2022-02-24 16:35:18 +0200

    update

--- main.js
+++ main.js
@@ -7,11 +7,33 @@ class User {
         this.type = type
         this.password = password
         this.isLoggedIn = false 
+        this.lastLoggedIn = 0
         this.isActive = true
     }
-
+    signIn(){
+        if (this.isActive) {
+            this.isLoggedIn = true
+            this.lastLoggedIn = Date.now()
+        } else {
+            alert('This account has been deleted')
+        }
+    }
+    signOut(){
+        this.isLoggedIn = false
+    }
+    getLastLoggedIn(){
+        const date = new Date(this.lastLoggedIn)
+        return date.toLocaleTimeString()
+    }
+    deactivateUser(){
+        this.isActive = false
+    }
+    reactivateUser(){
+        this.isActive = true
+        alert('Welcome Back '+ this.firstName)
+    }
 }
 
-const user = new User ('Ibrahim','Sharif','22','@sharif','student','test123')
-
-console.log(user);
+const u1 = new User ('Ibrahim','Sharif','22','@sharif','student','test123')
+u1.signIn()
+console.log(u1);
